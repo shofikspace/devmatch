1142c6f4003527dcada6ac3643f0b979cf0c7745
shofikul Islam <mohammadshofik@gmail.com>
2020-08-28 12:06:50 +0000

    Added contact model file

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 20200828092227) do
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "comments"
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
