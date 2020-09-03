9bfc000a1a4172368f567053966f23f7a5bde64a
shofikul Islam <mohammadshofik@gmail.com>
2020-09-03 08:30:35 +0000

    Added code comment

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,43 @@
 class ContactsController < ApplicationController
+  
+  # Get request to /contact-us
+  # Show new contact form
+  
     def new
       @contact = Contact.new
     end
     
+    # Post request /contacts
+    
     def create
+      
+      # Mass assignment of form fields into Contact object
       @contact = Contact.new(contact_params)
+      # Save the contact object into th edatabase
       if @contact.save
+        # Store form field via parameters, into variables 
         name = params[:contact][:name]
         email = params[:contact][:email]
         body = params[:contact][:comments]
+        # Plug variables into Contact Mailer 
+        # email method and send email
         ContactMailer.contact_email(name, email, body).deliver
+        # Store success message in flash hash
+        # and redirect to the new action
         flash[:success] = "Messege sent"
         redirect_to new_contact_path
       else
+        # If contact object doesnt save,
+        # store error in flash hash,
+        # and redirect to the new action
         flash[:danger] = @contact.errors.full_messages.join(", ")
         redirect_to new_contact_path
       end 
     end
+    
     private
+    # To collect data from form, we need to use strrong parameters
+    # Whitelist the from fields
     def contact_params
      params.require(:contact).permit(:name, :email, :comments)
     end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    # Contatct form validations
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
