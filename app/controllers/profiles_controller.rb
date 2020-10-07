a33f819d651bdd25aee62d6830e4a2f33c9dc24b
shofikul Islam <mohammadshofik@gmail.com>
2020-10-07 15:42:03 +0000

    Added user profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
     # GET to /users/:user_id/profile/new
     def new
     #Render blank profile form
-        
+     @profile = Profile.new
     end
 end
